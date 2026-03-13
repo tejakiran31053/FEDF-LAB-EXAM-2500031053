00a5656c770036d7c7d543eb3a587a56c181a0bf
Teja <2500031053@kluniversity.in>
2026-03-13 12:18:51 +0530

    modified Navbar.css

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>My topic is Farmer Portal</h1>
-      <p>In this application, we can register farmer details and also the API using axios of fake url given by sir.</p>
+      <h1>I am in Home Page</h1>
+      <h2>My topic is Farmer Portal</h2>
+      <p>In this application, we can register farmer details  also the API using axios of fake url given by sir.</p>
     </div>
   );
 };
--- src/exam/Navbar.jsx
+++ src/exam/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Farmer Portal</h2>
-
-      <NavLink to="/">Home</NavLink> |
-      <NavLink to="/register"> FamerRegistration</NavLink> |
-      <NavLink to="/display"> FarmerDisplay</NavLink> |
+    <nav className="navbar">
+  
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/register"> FarmerRegistration</NavLink>
+      <NavLink to="/display"> FarmerDisplay</NavLink>
       <NavLink to="/api"> ApiDemo</NavLink>
 
     </nav>
